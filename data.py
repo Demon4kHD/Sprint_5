28cfa4c8d71843a59f6d3077f68afffb9b48ae42
Dmitriy <d@demon4k.ru>
2024-09-02 00:58:43 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 
 symbols_for_password = '0123456789abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
-users_login = 'demidov13'
+users_kagorta = '13'
 users_domain = '@ya.ru'
-users_name = ['Dmitriy', 'Kseniya', 'Nikolay', 'Ivan']
-
+users_name = ['Dmitriy', 'Kseniya', 'Alexander', 'Nikita']
+user_soname = ['Demidov', 'Luscheva', 'Orekhov', 'Oliukov']
