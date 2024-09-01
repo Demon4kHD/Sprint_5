6aaff3cc66170573b8f06ce813e4a89338299a74
Dmitriy <d@demon4k.ru>
2024-09-02 00:58:08 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,10 +1,10 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import locators
 import links
 import data_generator
-import time
 import random
 
 
@@ -15,20 +15,27 @@ def start_driver():
     driver.quit()
 
 @pytest.fixture
-def registration_user():
-    driver = webdriver.Chrome()
+def registration_user(start_driver):
+    driver = start_driver
     driver.get(links.REGISTRATION_URL)
+
     user_data = data_generator.create_valid_email_and_password(random.randint(6, 12))
-    time.sleep(1)
-    driver.find_element(By.XPATH, locators.RegistrationPageElements.REG_INPUT_NAME).send_keys(user_data[0])
-    driver.find_element(By.XPATH, locators.RegistrationPageElements.REG_INPUT_EMAIL).send_keys(user_data[1])
-    driver.find_element(By.XPATH, locators.RegistrationPageElements.REG_INPUT_PASSWORD).send_keys(
-        user_data[2])
-    driver.find_element(By.XPATH, locators.RegistrationPageElements.REG_BUTTON_GET_REG).click()
-    time.sleep(1)
-    driver.find_element(By.XPATH, locators.AuthorizationPageElements.AUTH_INPUT_EMAIL).send_keys(user_data[1])
-    driver.find_element(By.XPATH, locators.AuthorizationPageElements.AUTH_INPUT_PASSWORD).send_keys(user_data[2])
-    driver.find_element(By.XPATH, locators.AuthorizationPageElements.AUTH_BUTTON_GET_AUTH).click()
-    time.sleep(1)
-    yield driver
-    driver.quit()
+    driver.find_element(*locators.RegistrationPageElements.REG_INPUT_NAME).send_keys(user_data[0])
+    driver.find_element(*locators.RegistrationPageElements.REG_INPUT_EMAIL).send_keys(user_data[1])
+    driver.find_element(*locators.RegistrationPageElements.REG_INPUT_PASSWORD).send_keys(user_data[2])
+    WebDriverWait(driver, 20).until(EC.visibility_of_element_located(
+        locators.RegistrationPageElements.REG_BUTTON_GET_REG)).click()
+
+    return driver, user_data
+
+@pytest.fixture
+def authorization_user(registration_user):
+    driver, user_data = registration_user
+    WebDriverWait(driver, 20).until(EC.visibility_of_element_located(
+        locators.AuthorizationPageElements.AUTH_H2_ENTRANCE))
+
+    driver.find_element(*locators.AuthorizationPageElements.AUTH_INPUT_EMAIL).send_keys(user_data[1])
+    driver.find_element(*locators.AuthorizationPageElements.AUTH_INPUT_PASSWORD).send_keys(user_data[2])
+    driver.find_element(*locators.AuthorizationPageElements.AUTH_BUTTON_GET_AUTH).click()
+
+    return driver
