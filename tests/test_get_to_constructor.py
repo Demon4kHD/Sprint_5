fbba0de2fb092cd2151d054b9db2a13f41d1ccd7
Dmitriy <d@demon4k.ru>
2024-09-02 01:03:58 +0300

    Update test_get_to_constructor.py

--- tests/test_get_to_constructor.py
+++ tests/test_get_to_constructor.py
@@ -1,17 +1,19 @@
 import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
-import data
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import links
 import locators
 
 class TestGetToConstructor:
     @pytest.mark.parametrize('list_locators',
                              [locators.AllPagesElements.TOPLINE_CONSTRUCTOR, locators.AllPagesElements.TOPLINE_LOGO])
-    def test_authorize_user_click_to_constuctor_and_logo(self, list_locators, registration_user):
-        self.driver = registration_user
-        self.driver.find_element(By.XPATH, list_locators).click()
-        time.sleep(1)
+    def test_from_page_profile_authorize_user_click_to_constuctor_and_logo(self, list_locators, authorization_user):
+        driver = authorization_user
 
-        assert self.driver.current_url == links.STELLAR_BURGERS_URL
+        driver.find_element(*locators.AllPagesElements.TOPLINE_ACCOUNT).click()
+        WebDriverWait(driver, 20).until(EC.visibility_of_element_located(
+            locators.PersonalAccountElements.ACC_EXIT_BUTTON))
+        driver.find_element(*list_locators).click()
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable(locators.MainPageElements.MAIN_SAUCE_GROUP))
+
+        assert driver.current_url == links.STELLAR_BURGERS_URL
